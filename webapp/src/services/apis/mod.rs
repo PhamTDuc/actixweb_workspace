866f0a68e7ee7e8bfc067370d7a3d92d1ffbe690
Pham Trung Duc <ducpt@valofe.com>
2023-02-10 16:32:53 +0700

    * Can't deserialize enum type

--- webapp/src/services/apis/mod.rs
+++ webapp/src/services/apis/mod.rs
@@ -1,16 +1,15 @@
 pub mod admin;
-
+use log::info;
 use actix_session::Session;
 use actix_web::{Responder, get, post, web::{self, Data}, Error, error, http::StatusCode, HttpRequest, HttpResponse};
 use authentication::claims::Claims;
 use serde::Deserialize;
-use crate::{services::models, config::AppData};
+use crate::{services::models::{self, response::{PermissionRole}}, config::AppData};
 use actix_web_grants::proc_macro::{ has_permissions, has_any_permission};
 
 #[derive(Deserialize)]
-pub struct UserPermissions {
+pub struct User {
     pub username: String,
-    pub permissions: Vec<String>,
 }
 
 #[get("/")]
@@ -33,24 +32,27 @@ pub async fn get_ready_role()->impl Responder{
 
 
 #[post("/login")]
-pub async fn login(req: HttpRequest, info: web::Json<UserPermissions>)->impl Responder{
+pub async fn login(req: HttpRequest, info: web::Json<User>)->impl Responder{
     let app_data = req.app_data::<Data<AppData>>().unwrap();
     
-    let query = sqlx::query("SELECT role FROM authentication.user_info WHERE user_name=$1")
+    let query = sqlx::query_as::<_, PermissionRole>("
+        WITH user_role as (SELECT role FROM authentication.user_info WHERE user_name=$1)
+        SELECT * FROM authentication.permission_role
+        WHERE role=(SELECT role FROM user_role)")
     .bind(&info.username)
     .fetch_one(&app_data.pool).await;
 
-    if let Ok(_)= query{      
+    if let Ok(role)= query{      
         let user_info = info.into_inner();
-        let claims = Claims::new(user_info.username, user_info.permissions, 1);
+        let claims = Claims::new(user_info.username, role.permission.clone().into_iter().map(|permission| permission.into()).collect(), 1);
        
         let jwt_secret = dotenvy::var("JWT_SECRET");
         if let Ok(jwt_secret) = &jwt_secret{
             let jwt =  Claims::create_jwt(jwt_secret, claims);
             if let Ok(jwt) = &jwt{
-                return HttpResponse::Ok().body(jwt.to_owned());
+                return HttpResponse::Ok().body(serde_json::to_string(&role).unwrap());
             }
         }
-    }   
-    return HttpResponse::InternalServerError().into();
+    }
+    return HttpResponse::Forbidden().into();
 }
--- webapp/src/services/models/response.rs
+++ webapp/src/services/models/response.rs
@@ -1,14 +1,54 @@
-use serde::Serialize;
+use serde::{Serialize};
+use sqlx::{Type, postgres::{PgHasArrayType, PgTypeInfo}};
 
 #[derive(Serialize)]
 pub struct Status{
     pub status: String
 }
 
-struct UserInfo{
-    user_name:  String,
-    email: String,
-    password: String,
-    role: String,
-    status: String
+#[derive(sqlx::FromRow)]
+pub struct UserInfo{
+    pub user_name:  String,
+    pub email: String,
+    pub password: String,
+    pub role: String,
+    pub status: String
+}
+
+#[derive(sqlx::FromRow, Serialize)]
+pub struct PermissionRole{
+    pub role:Role,
+    pub permission: Vec<Permission>
+}
+ 
+#[derive(sqlx::Type, Serialize, Clone)]
+#[sqlx(type_name="role")]
+#[sqlx(rename_all="lowercase")]
+pub enum Role{
+    Admin,
+    User
+}
+
+#[derive(sqlx::Type, Serialize, Clone)]
+#[sqlx(type_name="permission")]
+#[sqlx(rename_all="lowercase")]
+pub enum Permission{
+
+    Grant_Permission,
+    Can_View
+}
+
+impl From<Permission> for String{
+    fn from(value: Permission) -> Self {
+        match value {
+            Permission::Can_View=> "can_view".to_owned(),
+            Permission::Grant_Permission => "grant_permission".to_owned(),
+        }
+    }
+}
+
+impl PgHasArrayType for Permission{
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        PgTypeInfo::with_name("permission")
+    }
 }
